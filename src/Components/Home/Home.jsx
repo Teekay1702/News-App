6aec94f0cd6171bdaeeb0f7669cbe3a63688ead4
Teekay1702 <tokoloho57@gmail.com>
2025-03-25 10:34:09 +0200

    changed the api key

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import { useEffect, useState} from 'react';
 import './Home.css'
 
 const Home = () => {
-    return (
-        <div className="home">
-            <h1>Home</h1>
-        </div>
-    )
+    const [articles, setArticles] = useState([]);
+    const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
+    console.log(API_KEY);
+
+    useEffect(() => {
+        const fetchNews = async () => {
+            try {
+                const response = await fetch(
+                    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+                );
+                const data = await response.json();
+                setArticles(data.articles);
+            }
+            catch (error) {
+                console.error("Error fetching news:", error);
+            }
+        }
+        fetchNews();
+    }, [])
+
+     return (
+    <div>
+      <h1>Latest News</h1>
+      <ul>
+        {articles.map((article, index) => (
+          <li key={index}>{article.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
