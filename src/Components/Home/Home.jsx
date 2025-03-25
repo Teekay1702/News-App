a50132d4e9b64a8f60e1a47f016289377687523c
Teekay1702 <tokoloho57@gmail.com>
2025-03-25 12:00:39 +0200

    added spinner

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,16 +4,20 @@ import './Home.css'
 
 const Home = ({selectedCategory}) => {
 	const [articles, setArticles] = useState([]);
+	const [loading, setLoading] = useState(true);
 	const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
 	useEffect(() => {
 		const fetchNews = async () => {
 			try {
+				setLoading(true);
 				const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${selectedCategory}&apiKey=${API_KEY}`);
 				const data = await response.json();
 				setArticles(data.articles);
 			} catch (error) {
 				console.error("Error fetching news:", error);
+			} finally {
+				setLoading(false);
 			}
 		};
 		fetchNews();
@@ -21,15 +25,25 @@ const Home = ({selectedCategory}) => {
 
 	return (
 		<div>
-			<h1>Latest {selectedCategory.charAt(0).toUpperCase() + selectedCategory.slice(1)} News</h1>
-			<div className="news-list">
-				{
-				articles.map((article, index) => (
-					<NewsCard key={index}
-						article={article}/>
-				))
-			} </div>
-		</div>
+			<h1>Latest {
+				selectedCategory.charAt(0).toUpperCase() + selectedCategory.slice(1)
+			}
+				News</h1>
+			{
+			loading ? (
+				<div className="spinner-container">
+					<div className="spinner"></div>
+				</div>
+			) : (
+				<div className="news-list">
+					{
+					articles.map((article, index) => (
+						<NewsCard key={index}
+							article={article}/>
+					))
+				} </div>
+			)
+		} </div>
 	);
 }
 
